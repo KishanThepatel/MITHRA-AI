c74632fd133f708f8a071230604593b1b8dc1008
Kishan Patel <seekkishanpatel@gmail.com>
2024-10-17 13:11:36 +0530

    initial commit

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
+r = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis...")  
+    while True:
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = r.listen(source, timeout = 2)  
+
+    print("Recognizing...")
+    
+
+     
+    
