54ffc00d6ee0266ed16b3288b3f39427601c038b
Kishan Patel <seekkishanpatel@gmail.com>
2024-10-25 19:53:42 +0530

    fourth commit

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import pyttsx3
 from decouple import config
 from datetime import datetime
 import keyboard
+import requests
 
 loading_call_list =[
     "just a second"
 ]
+
+listening = False
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
@@ -20,36 +23,6 @@ USER = config('USER', default = 'Kishan')
 HOSTNAME = config('BOT', default = 'mythhra')
 newsApi = "386fb4f3ba594671b96b222e6121f072"
 
-
-def greet():
-    hour = datetime.now().hour
-    if(hour>=6 and hour<=12):
-        speak(f"Good morning {USER}")
-    elif(hour>=12 and hour<=16):
-        speak(f"Good afternoon {USER}")
-    elif(hour>=16 and hour<=19):
-        speak(f"good evening {USER}")
-    speak(f"{HOSTNAME} here, how may I assist you?")  
-    print('to initialize press ctrl+alt+m')      
-
-
-listening = False    
-    
-def start_listening():
-    global listening
-    listening = True
-    print('listening...')
-
-def pause_listening():
-    global  listening
-    listening = False
-    print('shutting off') 
-    speak('shutting off')
-    
-keyboard.add_hotkey('ctrl+alt+m', start_listening)
-keyboard.add_hotkey('ctrl+alt+i', pause_listening)                        
-
-
 def speak(text):
     engine.say(text)
     engine.runAndWait()
@@ -58,51 +31,82 @@ def song(songName):
     webbrowser.open(f"https://www.youtube.com/results?search_query={songName}")
 
 def take_command():
+    global listening
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 1
-        audio = r.listen(source)
+        audio = r.listen(source, timeout=4, phrase_time_limit=3)
+ 
+        try:
+            queri = r.recognize_google(audio, language='en-in').lower()
+            print(f"Recognized: {queri}")
 
-    try:
-        print("Recognizing....")
-        queri = r.recognize_google(audio, language='en-in')
-        print(queri)
-        if not 'exit' in queri or 'stop' in queri:
-            speak(loading_call_list)
-        else:
-            hour = datetime.now().hour
-            if hour >= 21 and hour < 6:
-                speak("Good night take care")
+            if 'mitra' in queri:
+                listening = True
+                queri = queri.replace('mitra', "").strip()
+                return queri
+
+            if 'exit' in queri or 'stop' in queri:                                    
+                hour = datetime.now().hour
+                listening = False
+                if hour >= 21 and hour < 6:
+                    speak("Good night take care")
+                else:
+                    speak("Have a good day")
+                exit()
+
+        except sr.UnknownValueError:
+                speak("Couldn't catch that. Please repeat.")
+                print("Couldn't catch that. Please repeat.")
+                return None
+        except sr.RequestError as e:
+                speak("Could not request results; check your internet connection.")
+                print("Could not request results; {0}".format(e))
+                return None
+
+    return None        
+            
+
+def handle_command(query):
+    if query:
+        if "how are you" in query:
+            speak("I am absolutely fine")  
+
+        elif 'mitra' in query:
+            speak('yes')               
+
+        elif "open" in query:
+            query = query.replace("open", "").strip()
+            webbrowser.open(f"https://{query}.com")
+            speak(f"opening {query}")
+            
+                    
+        elif "play" in query:
+            songName = query.replace("play", "").strip()
+            song(songName)
+
+        elif "news" in query:
+            webbrowser.open("https://www.google.com/search?q=news")  
+            news = requests.get(f'https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey={newsApi}')
+            data = news.json()
+            if data.get('status') == 'ok':
+                for article in data['articles']:
+                    print(article['title'])
+                    speak(article['title'])
             else:
-                speak("Have a good day")
-            exit()
-
-    except Exception:
-        speak("Sorry I couldn't understand. Can you please repeat that?")
-        queri = 'None'
-    return queri
-
+                speak('failed to retrieve recent news')              
 
 if __name__ == '__main__':
-    greet()
     while True:
-        if listening:
-            query = take_command().lower()
-            if "how are you" in query:
-                speak("I am absolutely fine sir. What about you")  
+        if not listening:
+            trigger_word = take_command()
+            if trigger_word:
+                handle_command(trigger_word)
+        else:
+            query = take_command()
+            handle_command(query)
+            listening = False                
 
-            elif "open" in query:
-                query = query.replace("open", "").strip()
-                webbrowser.open(f"https://{query}.com")
-                speak(f"opening {query}")
-        
-                
-            elif "play" in query:
-                songName = query.replace("play", "").strip()
-                song(songName)
-
-            elif "news" in query:
-                webbrowser.open("https://www.google.com/search?q=news")  
+             
 
                
 
